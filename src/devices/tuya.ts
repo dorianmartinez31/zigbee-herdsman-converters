cefdd1d5b94dbba9d2a13e4cc99a062a8f548382
dorianmartinez31 <74448909+dorianmartinez31@users.noreply.github.com>
2025-02-06 23:05:35 +0100

    Update tuya.ts
    Manage TS000F _TZ3000_hdc8bbha and TS000F _TZ3218_hdc8bbha
    May also fix issue https://github.com/Koenkk/zigbee2mqtt/issues/25789

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -9331,6 +9331,22 @@ const definitions: DefinitionWithExtend[] = [
         configure: async (device, coordinatorEndpoint) => {
             await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
             device.powerSource = 'Mains (single phase)';
+            // Device advertises itself as Router but is an EndDevice
+            device.type = 'EndDevice';
+            device.save();
+        },
+    },
+    {
+        fingerprint: tuya.fingerprint('TS000F', ['_TZ3218_hdc8bbha']),
+        model: 'QS-Zigbee-SEC01-DC',
+        vendor: 'Tuya',
+        description: 'Min 1 gang Zigbee switch module',
+        extend: [tuya.modernExtend.tuyaOnOff({switchType: true})],
+        configure: async (device, coordinatorEndpoint) => {
+            await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
+            device.powerSource = 'Mains (single phase)';
+            // Device advertises itself as Router but is an EndDevice
+            device.type = 'EndDevice';
             device.save();
         },
     },
